e5a9b4214cfd5e15a6c83043a2e617f37b9e1303
0xV1c10u5 <206298950+0xV1c10u5@users.noreply.github.com>
2025-09-18 21:42:24 -0700

    added ticker lookup lambda

--- app/app.py
+++ app/app.py
@@ -4,5 +4,11 @@
 
 ## ! Also, add a profiler here
 
-# Function Imports
-import 
+# Populate Data Function
+import yfinance as yf
+
+def population(ticker):
+    data = yf.Ticker(ticker)
+    return data.info
+
+print(population("MSFT"))
